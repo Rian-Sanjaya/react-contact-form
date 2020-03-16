0a9c9d99feb984af936ee981a77cb6d157565d26
Rian <rian.sj@gmail.com>
2020-03-16 12:41:08 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ContactForm from './components/ContactForm'
+import { Message } from './components/Message'
+import { UserPanel } from './components/UserPanel'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  CONTACT_FORM_DEFAULTS = {
+    name: '',
+    email: '',
+    option: 'A',
+    select: 1,
+    type: '',
+    message: '',
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      contact: {...this.CONTACT_FORM_DEFAULTS},
+      sent: false,
+      currentUser: null,
+    }
+  }
+
+  logIn = () => {
+    this.setState({
+      currentUser: {
+        name: "Test User",
+        email: "user@example.com"
+      },
+      contact: {
+        ...this.CONTACT_FORM_DEFAULTS,
+        name: "Test User",
+        email: "user@example.com"
+      }
+    })
+  }
+
+  contactChanged(contact) {
+    this.setState({
+      contact
+    })
+  }
+
+  sendContact(contact) {
+    this.setState({
+      sent: true,
+      contact
+    })
+  }
+
+  render() {
+    const { sent, contact, currentUser } = this.state
+
+    if (sent)
+      return (
+        <Message 
+          header={`Thank You ${contact.name}`}
+          text={`We will reply to your message in next 24h. Have a nice day! ;-)`}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          We will get back to you. Have a nice day.
+        </Message>
+      )
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="pull-right">
+              <button className="btn btn-default" onClick={this.logIn}>
+                <i className="glyphicon glyphicon-user"></i>Log In
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <h2>Contact us</h2>
+            {
+              currentUser && 
+              <UserPanel user={currentUser} />
+            }
+            <p>Please fill in form on the right to get fast reply</p>
+            <img style={{width: '100%'}} src="http://via.placeholder.com/300x200" alt="" />
+          </div>
+          <div className="col-md-8">
+            <ContactForm 
+              data={contact} 
+              onChange={ contact => (
+                this.contactChanged(contact)
+              )}
+              onSubmit={ contact => (
+                this.sendContact(contact)
+              )} 
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Message.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Message = props => {
+  return (
+    <div className="text-center">
+      {
+        props.header && 
+        <h3 className="message-header">
+          {props.header}
+        </h3>
+      }
+
+      <div className="message-body">
+        {
+          props.text ? props.text : props.children
+        }
+      </div>
+    </div>
+  )
+}
